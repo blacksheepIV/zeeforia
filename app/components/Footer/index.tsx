5caeb787107567efc970d4d751cba9a95c4cfff2
blackSheep777 <forough.fallahfar@gmail.com>
2025-01-10 03:56:30 +0300

    :bug: Footer
    fix footer problem

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,13 +1,17 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 function Footer() {
-  const [today, setToday] = useState(new Date())
+  const [today, setToday] = useState<Date>()
+  useEffect(() => {
+    const currentDate = new Date()
+    setToday(currentDate)
+  }, [])
   return (
     <div className="absolute bottom-0 left-0 w-full h-32 py-4 px-6 flex flex-col justify-center items-center bg-white shadow-custom">
       {/* <div className="h-[0.5px] w-[50%] bg-fade-gradiant mb-3"></div> */}
       <p className="font-semibold">
-        © {today.getFullYear()} Zeeforia. All rights reserved.{' '}
+        © {today?.getFullYear()} Zeeforia. All rights reserved.
       </p>
       <p>Made with love and pixels by Foroogh.</p>
     </div>
