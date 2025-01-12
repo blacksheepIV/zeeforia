c8107beef04ce3551a6f4a89a65452ed93290c66
blackSheep777 <forough.fallahfar@gmail.com>
2025-01-12 04:54:21 +0300

    :sparkles: About page
    add about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,57 @@
+import Image from 'next/image'
 import React from 'react'
 
 function AboutPage() {
-  return <div className="container">AboutPage</div>
+  return (
+    <section className="container mx-auto">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 justify-between items-center font-montserrat py-8 px-4">
+        <div className="flex flex-col items-start gap-2 lg:order-1 order-2">
+          <h2 className="font-semibold text-xl text-raisinBlack-600">
+            About Zeef Oria
+          </h2>
+          <p className="text-base sm:text-md lg:text-base text-raisinBlack-400 text-justify">
+            Born in 1972, Zeef Oria is a contemporary artist whose journey in
+            art began in his twenties. A remarkable talent emerged during these
+            early years, earning him 3rd place in the prestigious (Visual Arts
+            Exhibition). This recognition was a glimpse into the boundless
+            potential of an artist deeply connected to his craft. However, life
+            took him on a path away from the canvas, and for 30 years, silence
+            enveloped his artistic expression. Yet this silence was not a
+            void—it was a space of transformation, observation, and growth.
+            Through decades of experiences, both personal and universal, Zeef
+            Oria amassed a reservoir of stories, emotions, and insights waiting
+            to take form.
+          </p>
+          <p className="text-base sm:text-lg lg:text-base text-raisinBlack-400 text-justify">
+            Now, after three decades, Zeef returns to his passion with a renewed
+            vision. This period of absence has become a wellspring of
+            inspiration, allowing him to channel a lifetime of experiences into
+            his work. His art speaks of time, resilience, and the beauty found
+            in rediscovery. Each piece is an exploration of memory, silence, and
+            the rich tapestry of human experience, imbued with a profound
+            understanding of life’s complexities.
+          </p>
+          <p className="text-base sm:text-lg lg:text-base text-raisinBlack-400 text-justify">
+            Zeef Oria’s return to the art world is not just a continuation of
+            his journey; it is a renaissance. With every brushstroke and every
+            piece, he transforms the echoes of his 30-year silence into a
+            powerful dialogue, offering viewers a glimpse into a soul that has
+            lived, endured, and now creates anew.
+          </p>
+        </div>
+
+        <div className="lg:order-2 order-1 lg:px-8 lg:row-span-2">
+          <Image
+            src="/about-img.jpg"
+            alt="Zeef oria's photo"
+            width={1200}
+            height={1000}
+            className="filter hover:grayscale-0 sm:grayscale-0 lg:grayscale transition duration-300"
+          />
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default AboutPage
--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -8,12 +8,21 @@ function Footer() {
     setToday(currentDate)
   }, [])
   return (
-    <div className="absolute bottom-0 left-0 w-full h-32 font-sans py-4 pb-1 px-6 flex flex-col justify-center items-center bg-white shadow-custom">
+    <div className="fixed bottom-0 left-0 w-full h-32 font-sans py-4 pb-1 px-6 flex flex-col gap-2 justify-center items-center bg-white shadow-custom">
       {/* <div className="h-[0.5px] w-[50%] bg-fade-gradiant mb-3"></div> */}
       <p className="font-semibold">
         © {today?.getFullYear()} Zeef oria. All rights reserved.
       </p>
-      <p className="text-sm">Made with love and pixels by Foroogh.</p>
+      <p className="text-sm">
+        Made with love and pixels by{' '}
+        <a
+          href="https://www.forough.dev"
+          target="_blank"
+          className="text-delftBlue-300"
+        >
+          Foroogh.
+        </a>
+      </p>
     </div>
   )
 }
