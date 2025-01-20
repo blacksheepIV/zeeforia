2b10534bbd139593c55eac78cabe38a3763f819b
blackSheep777 <forough.fallahfar@gmail.com>
2025-01-20 09:09:27 +0300

    :bug: Hero section
    fix hero section problem in production

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         autoPlay
         loop
         muted
-        className="w-[auto] h-screen object-cover"
+        className="w-full h-screen object-cover"
       >
         Your browser does not support the video tag.
       </video>
@@ -17,41 +17,12 @@ export default function Home() {
         <h1 className="sm:text-4xl md:text-6xl font-bold text-white animate-slide-in-left">
           Zeef Oria
         </h1>
-        <p className="sm:text-lg md:text-xl text-white delay-1000 animate-slide-in-left mt-5">
+        <p className="sm:text-lg md:text-xl text-white animate-slide-in-left mt-5">
           Discover Zeef Oria’s journey from silence to a vibrant artistic
           revival, celebrating memory, resilience, and rediscovery.
         </p>
       </div>
       <AboutSection />
-      {/* <div className="relative container mx-auto px-4 pt-20 pb-32">
-        <div className="grid lg:grid-cols-2 gap-12 items-center">
-          <div className="space-y-8 animate-fade-up">
-            <h1 className="font-montserrat text-6xl lg:text-7xl font-bold text-delft_blue">
-              Zeef <span className="text-amaranth_purple">Oria</span>
-            </h1>
-            <p className="font-quicksand text-xl text-gray-700 max-w-lg">
-              Discover the inspiring journey of Zeef Oria, a contemporary artist
-              who transforms three decades of silence into a vibrant artistic
-              renaissance. Explore his evocative works that delve into memory,
-              resilience, and the profound beauty of rediscovery.
-            </p>
-            <button className="bg-delft_blue text-white px-8 py-3 rounded-lg font-quicksand font-medium hover:bg-opacity-90 transition-colors">
-              View Gallery
-            </button>
-          </div>
-
-          <div className="relative">
-            <div className="absolute md:top-[22px] md:left-[10px] w-full md:min-h-[500px] top-2 left-2 min-h-full  hero-image rounded-lg shadow-2xl overflow-hidden bg-french_gray"></div>
-            <div className="hero-image rounded-lg shadow-2xl overflow-hidden">
-              <img
-                src="/cover.png"
-                alt="Geometric Art Piece"
-                className="w-full h-auto object-cover"
-              />
-            </div>
-          </div>
-        </div>
-      </div> */}
     </main>
   )
 }
