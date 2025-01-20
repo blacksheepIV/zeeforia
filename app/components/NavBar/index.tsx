33c1315e306da2a66bd8d1d2a8073be4a820eaac
blackSheep777 <forough.fallahfar@gmail.com>
2025-01-20 07:25:07 +0300

    :bug: Build error
    fix build error

--- app/components/NavBar/index.tsx
+++ app/components/NavBar/index.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link'
-import Image from 'next/image'
+//import Image from 'next/image'
 import React, { useState, useEffect } from 'react'
 import { navbarItems } from './constants'
 import { usePathname } from 'next/navigation'
