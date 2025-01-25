7c332727d06d9a9f113769c6c60663bc5778c151
blackSheep777 <forough.fallahfar@gmail.com>
2025-01-25 15:37:34 +0300

    :sparkles: Social links
    add social links add social links to footer

--- app/components/Footer/index.tsx
+++ app/components/Footer/index.tsx
@@ -1,5 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
+import { EmailIcon, InstagramIcon, YoutubeIcon } from '@/app/components/Icons'
 
 function Footer() {
   const [today, setToday] = useState<Date>()
@@ -8,11 +9,26 @@ function Footer() {
     setToday(currentDate)
   }, [])
   return (
-    <div className="w-full h-32 font-quicksand py-4 pb-1 px-6 flex flex-col gap-2 justify-center items-center bg-white shadow-custom">
+    <div className="w-full h-32 font-quicksand pt-4  px-6 flex flex-col justify-center items-center bg-white shadow-custom">
       {/* <div className="h-[0.5px] w-[50%] bg-fade-gradiant mb-3"></div> */}
       <p className="font-semibold">
         © {today?.getFullYear()} Zeef oria. All rights reserved.
       </p>
+      <div className="mt-5 w-full flex gap-3 justify-center items-center">
+        <a href="https://www.instagram.com/zeef_oria/" target="_blank">
+          <InstagramIcon width={30} height={30} />
+        </a>
+        <a
+          href="https://youtube.com/@zeeforia?si=g-C1rerJMBhhyuWg"
+          target="_blank"
+        >
+          <YoutubeIcon width={30} height={30} />
+        </a>
+
+        <a href="mailto:art@zeeforia.com`" target="_blank">
+          <EmailIcon width={30} height={30} />
+        </a>
+      </div>
     </div>
   )
 }
